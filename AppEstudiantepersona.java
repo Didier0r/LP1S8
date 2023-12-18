aee428b77b4c5b653fe36ee088bf7d06c144884a
Didier0r <didierrecalde@gmail.com>
2023-12-18 18:02:49 -0300

    updated

---
+++ AppEstudiantepersona.java
@@ -0,0 +1,13 @@
+/**
+ * @author Didier Recalde
+*/
+ public class AppEstudiantepersona {
+    public static void main(String[] args) {
+     Estudiante e = new Estudiante("Luis Garcia", 20);
+     persona p = new Estudiante("Marta Gómez", 20);
+
+     e.mostrarDatos();
+     p.mostrarDatos();
+    
+    }
+    }
---
+++ Estudiante.java
@@ -0,0 +1,27 @@
+/**
+ * @author Didier Recalde
+*/
+public class Estudiante extends persona {
+
+   private int creditos;
+
+public Estudiante(String nombre, int edad){
+
+  super(nombre,edad);
+   this.creditos = 60;
+
+}
+ public int getcreditos(){
+     return this.creditos;
+ }
+
+  public void mostrarDatos(){
+   super.mostrarDatos();
+   System.out.println("Estudiante Matriculado de " +creditos+ " Creditos");
+
+  }
+
+
+
+
+}
